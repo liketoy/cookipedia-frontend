67c17045f4fa32eae14c5f660e4297e6a9f8b6fb
bewoody <woody@maycan.co.kr>
2022-12-22 19:26:06 +0900

    feature: Floating Button

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,11 +1,19 @@
+import { Icon, Stack } from "@chakra-ui/react";
+import FloatingButton from "../components/FloatingButton";
 import Layout from "../components/Layout";
 import ProtectedPage from "../components/ProtectedPage";
+import { FaPlus } from "react-icons/fa";
 
 export default function Home() {
   return (
     <ProtectedPage>
       <Layout title={"팬트리"} hasTabBar>
-        <h1>Home</h1>
+        <Stack direction={"column"}>
+          <h1>Home</h1>
+          <FloatingButton href="/user">
+            <Icon as={FaPlus} />
+          </FloatingButton>
+        </Stack>
       </Layout>
     </ProtectedPage>
   );
